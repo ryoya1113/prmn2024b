1b05162bf1cf6a3390ce64968671a9af660182aa
小岡凌弥 <ryoya.k1113@gmail.com>
2024-11-12 15:52:10 +0900

    プロジェクト作成

---
+++ prmn2024/src/main/java/org/example/prmn2024/Prmn2024Application.java
@@ -0,0 +1,13 @@
+package org.example.prmn2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2024Application.class, args);
+    }
+
+}
